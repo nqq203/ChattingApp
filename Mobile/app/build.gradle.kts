ba9fcc27c25298fe12374a26245c195c99c5fe49
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2024-02-18 00:32:37 +0700

    add message layout (dummy)

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,4 +40,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    Glide dependency
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+//    Shape dependency
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
