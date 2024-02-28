45f73c62aec897fc19d0cb4e7090bcaccfb7fb13
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2024-02-28 21:46:56 +0700

    add spring-app

---
+++ MatchMingle/src/test/java/com/matchmingle/MatchMingle/MatchMingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchmingle.MatchMingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
