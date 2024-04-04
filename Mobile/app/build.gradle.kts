9a3a400aaab4db7467f103239c6f2c3b158f74d6
linkatoh22 <lintoh111@gmail.com>
2024-04-04 18:18:03 +0700

    ADD: Data in Matches List

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -55,6 +56,8 @@ dependencies {
 //  Firebase config
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
-
+    implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-auth")
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
